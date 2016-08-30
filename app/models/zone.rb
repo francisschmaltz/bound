c0af6dff933369000304f128250090047f230449
Adam Cooke <me@adamcooke.io>
2016-08-30 15:09:33 +0100

    add a trailing dot on the $ORIGIN

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -71,7 +71,7 @@ class Zone < ApplicationRecord
       s << "; Zone file exported from Bound at #{Time.now.utc.to_s}\n"
       s << "; Bound Zone ID: #{id}\n\n"
       s << "$TTL".ljust(ZF_NAME_SPACE, ' ') + " #{self.ttl}\n"
-      s << "$ORIGIN".ljust(ZF_NAME_SPACE, ' ') + " #{self.name}\n\n"
+      s << "$ORIGIN".ljust(ZF_NAME_SPACE, ' ') + " #{self.name}.\n\n"
       s << "@".ljust(ZF_NAME_SPACE, ' ') + " "
       s << "IN".ljust(ZF_CLASS_SPACE, ' ')
       s << "SOA".ljust(ZF_TYPE_SPACE, ' ') + " "
