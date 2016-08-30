2c4f853d23b5468383630feff412999760a92676
Adam Cooke <me@adamcooke.io>
2016-08-30 15:06:08 +0100

    change to port 3000 by default in puma

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,7 @@ threads_count = Bound.config.web_server&.max_threads&.to_i || 5
 threads         threads_count, threads_count
 
 bind_address  = Bound.config.web_server&.bind_address || '127.0.0.1'
-bind_port     = Bound.config.web_server&.port&.to_i || 5000
+bind_port     = Bound.config.web_server&.port&.to_i || 3000
 bind            "tcp://#{bind_address}:#{bind_port}"
 
 environment     Bound.config.rails&.environment || 'development'
