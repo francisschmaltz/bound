69479b1d358ce4eeb8ca34792278dcb62f69a060
Francis Schmaltz <francis@yaboiii.com>
2021-10-02 21:19:18 +0000

    Update Zone Table

--- app/assets/javascripts/zone_record.js
+++ app/assets/javascripts/zone_record.js
@@ -2,14 +2,4 @@
 
   'use strict';
 
-  function handleClick(event) {
-    event.preventDefault();
-    var $row = $(event.currentTarget);
-    Turbolinks.visit($row.data('url'));
-  }
-
-  $(function () {
-    $(document).on('click', '.js-zoneRecord', handleClick);
-  });
-
 })();
